e24b9cee2b76b5f12fe545013e7dd2d8d7ad0795
fatouuuu <fatmaabdullrahim@gmail.com>
2023-03-01 10:14:01 +0300

    completed assignment

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,16 @@
 class Review < ActiveRecord::Base
+
+    # # establish relationship. a review belongs to a game
+    # def game 
+    #     # self is the review instance
+    #     # self.game_id is the game_id of the review instance
+    #     # Game.find(self.game_id) is the game instance
+    #     Game.find(self.game_id)
+    # end 
+    
+
+    # use macro instead of custom method: 
+    # belongs_to :game
+    belongs_to :game 
   
 end
